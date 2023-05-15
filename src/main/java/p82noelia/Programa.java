11e8876a0e28eadb335f6654e5d42ccbb898319b
Noelia <npergon914@g.educaand.es>
2023-05-16 00:43:10 +0200

    ,mhncvklnj

--- src/main/java/p82noelia/Programa.java
+++ src/main/java/p82noelia/Programa.java
@@ -4,12 +4,10 @@
  */
 package p82noelia;
 
+import controladores.FacturaJpaController;
 import entities.Factura;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 
 /**
  *
@@ -25,17 +23,10 @@ public class Programa {
     }
     
     public static List<Factura> findAll() {
-        EntityManager em = getEntityManager();
-        Query q = em.createNamedQuery("Factura.findAll");
-        List<Factura> listaF = (List<Factura>) q.getResultList();
-        em.close();
+        FacturaJpaController fjc = new FacturaJpaController(Persistence.createEntityManagerFactory("p81noeliap"));
+        List<Factura> listaF = fjc.findFacturaEntities();
         return listaF;
     }
-    
-    private static EntityManager getEntityManager() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("p81noeliap");
-        EntityManager em = entityManagerFactory.createEntityManager();
-        return em;
-    }
+
     
 }
--- target/generated-sources/annotations/entities/Factura_.java
+++ target/generated-sources/annotations/entities/Factura_.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-05-11T12:34:27", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-05-15T11:45:34", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Factura.class)
 public class Factura_ { 
 
